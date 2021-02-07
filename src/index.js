168f8fbc356aaf57754003e9a9ca0e053275f789
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-07 18:55:10 +0300

    feat: code update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   
+if (str.length % 2 !== 0) {return false}
+
   for (let i = 0; i <= str.length; i++) {
     for (let j = 0; j <= str.length; j++) {
       if(str[i] == bracketsConfig[i][j]) {        
