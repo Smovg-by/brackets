2a5cde84e65ad65a915cf9173713fdc5b8edefe8
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-07 09:29:20 +0300

    feat: JS Brackets code add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split("");
+  let openingCounter = 0;
+  let closingCounter = 0;
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+    for (j = 0; j < strArr.length; j++) {
+      if (bracketsConfig[i][0] == strArr[j]) {
+        ++openingCounter;
+      }
+    }
+  }
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+    for (j = 0; j < strArr.length; j++) {
+      if (bracketsConfig[i][1] == strArr[j]) {
+        ++closingCounter;
+      }
+    }
+  }
+
+  console.log(openingCounter, closingCounter);
+  if (openingCounter == closingCounter) {
+    return true;
+  } else {
+    return false;
+  }
 }
